01825a5253ee29aa0b89537840a8f69b5363053c
Varthyavath Lavanya <vl24csb0b83@student.nitw.ac.in>
2026-09-14 18:31:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Skills from "./pages/Skills";
+import About from "./pages/about";
+import Skills from "./pages/skills";
 import Projects from "./pages/Projects";
-import Education from "./pages/Education";
+import Education from "./pages/education";
 import Contact from "./pages/Contact";
 import ProjectDetails from "./pages/ProjectDetails";
 import NotFound from "./pages/NotFound";
